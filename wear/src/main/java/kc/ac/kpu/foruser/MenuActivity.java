d5ff4c117c41768e6ce238d62644a20d04a15598
seung-aa <baeka0202@gmail.com>
2020-02-25 14:12:29 +0900

    watch 화면 사이즈 맞춤

--- wear/src/main/java/kc/ac/kpu/foruser/MenuActivity.java
+++ wear/src/main/java/kc/ac/kpu/foruser/MenuActivity.java
@@ -12,47 +12,29 @@ import kc.ac.kpu.wear.R;
 public class MenuActivity extends WearableActivity {
 
     private TextView mTextView;
-    Button order = (Button)findViewById(R.id.order_btn); //주문하기버튼
-    Button order_check = (Button)findViewById(R.id.ordercheck_btn); //주문확인버튼
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        mTextView = (TextView) findViewById(R.id.text);
-
         // Enables Always-on
         setAmbientEnabled();
 
-        order.setOnClickListener(new View.OnClickListener() {
+        mTextView = (TextView) findViewById(R.id.text);
+        Button btn = (Button)findViewById(R.id.order_btn);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(intent);
-            }
-        });
-
-
-        order_check.setOnClickListener(new View.OnClickListener() {
-            @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(getApplicationContext(),OrderActivity.class);
-            startActivity(intent);
         }
 
-
-
-        });
-
-
-
-
-
-
-
-
+});
 }
 
 
-}
+    }
+
