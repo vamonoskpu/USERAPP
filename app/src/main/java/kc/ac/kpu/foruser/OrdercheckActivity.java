596ca94673901ec8898bcbd39479d7bdcb544115
seung-aa <baeka0202@gmail.com>
2020-05-05 12:30:27 +0900

    테스트파일

--- app/src/main/java/kc/ac/kpu/foruser/OrdercheckActivity.java
+++ app/src/main/java/kc/ac/kpu/foruser/OrdercheckActivity.java
@@ -21,7 +21,7 @@ public class OrdercheckActivity extends AppCompatActivity {
     DatabaseReference reference1;
     ImageView payment;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ordercheck);
 
