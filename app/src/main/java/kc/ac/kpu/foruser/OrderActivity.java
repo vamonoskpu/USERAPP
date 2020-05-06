99052536dec2cbdf166f62a91aef498c311d56ad
seung-aa <baeka0202@gmail.com>
2020-05-07 00:56:47 +0900

    wav녹음파일 헤더 추가

--- app/src/main/java/kc/ac/kpu/foruser/OrderActivity.java
+++ app/src/main/java/kc/ac/kpu/foruser/OrderActivity.java
@@ -4,11 +4,12 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -33,6 +34,9 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -41,8 +45,6 @@ public class OrderActivity extends AppCompatActivity {
 
 
     // Intent intent;
-    TextView result;
-    DatabaseReference databaseReference;
     FirebaseDatabase database;
     DatabaseReference reference;
     Button completebtn;
@@ -56,15 +58,10 @@ public class OrderActivity extends AppCompatActivity {
     TextView textView;
 
 
-
-
     //음성입력부분
 
     private Button mrecordBtn;
     private TextView mRecordLabel;
-
-    private MediaRecorder recorder;
-    private String fileName = null;
     private String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
     private static final String LOG_TAG = "Record_log";
     private StorageReference mStorage;
@@ -73,6 +70,20 @@ public class OrderActivity extends AppCompatActivity {
     private int labelNumber;
     private DatabaseReference mdatabase = FirebaseDatabase.getInstance().getReference();
     private FirebaseStorage storage = FirebaseStorage.getInstance();
+
+
+    //음성새로수정
+    private static final int RECORDER_BPP = 16;
+    private static final String AUDIO_RECORDER_FILE_EXT_WAV = ".wav";
+    private static final String AUDIO_RECORDER_FORDER = "AudioRecorder";
+    private static final String AUDIO_RECORDER_TEMP_FILE = "record_temp.raw";
+    private static final int RECORDER_SAMPLERATE = 44100;
+    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_STEREO;
+    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    private AudioRecord recorder = null;
+    private int bufferSize = 0;
+    private Thread recordingThread = null;
+    private boolean isRecording = false;
 
 
     @SuppressLint("ClickableViewAccessibility")
@@ -100,14 +111,13 @@ public class OrderActivity extends AppCompatActivity {
 
         completebtn = findViewById(R.id.completebtn);
 
-        mStorage  = FirebaseStorage.getInstance().getReference();
+        mStorage = FirebaseStorage.getInstance().getReference();
         mRecordLabel = findViewById(R.id.recordlabel);
         mrecordBtn = findViewById(R.id.recordbtn);
         mProgress = new ProgressDialog(this);
 
-        fileName = Environment.getExternalStorageDirectory().getAbsolutePath();
 
-        fileName += "/recorded_audio.wav";
+        bufferSize = AudioRecord.getMinBufferSize(8000, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT);
 
 
         reference.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -176,8 +186,6 @@ public class OrderActivity extends AppCompatActivity {
         });
 
 
-
-
         //여기까지가 파이어베이스에서 값 가져오는 코드입니다.
 
 
@@ -187,9 +195,11 @@ public class OrderActivity extends AppCompatActivity {
 
 
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    isRecording = true;
                     startRecording();
                     mRecordLabel.setText("메뉴이름을 말씀해주세요.");
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    isRecording = false;
                     stopRecording();
                     mRecordLabel.setText("주문 완료");
                 }
@@ -200,52 +210,198 @@ public class OrderActivity extends AppCompatActivity {
 
     }
 
-    private void startRecording() {
-
-        recorder = new MediaRecorder();
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC); // 오디오 입력 지정(마이크)
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4); // 출력 형식 지정
-        //마이크로 들어오는 음성데이터는 용량이 크기 때문에 압축이 필요
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT); // 인코딩
-        recorder.setOutputFile(fileName); // 음성 데이터를 저장할 파일 지정
-
-        try {
-            recorder.prepare();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
+    private String getFilename() {
+        String filepath = Environment.getExternalStorageDirectory().getPath();
+        File file = new File(filepath, AUDIO_RECORDER_FORDER);
+        if (!file.exists()) {
+            file.mkdirs();
         }
 
-        recorder.start();
+        return (file.getAbsolutePath() + "/recored.wav");
+
     }
 
+
+    private String getTempFilename() {
+        String filepath = Environment.getExternalStorageDirectory().getPath();
+        File file = new File(filepath, AUDIO_RECORDER_FORDER);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        File tempFile = new File(filepath, AUDIO_RECORDER_TEMP_FILE);
+        if (tempFile.exists())
+            tempFile.delete();
+        return (file.getAbsolutePath() + "/" + AUDIO_RECORDER_TEMP_FILE);
+    }
+
+    private void startRecording() {
+        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, RECORDER_SAMPLERATE, RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING, bufferSize);
+
+        int i = recorder.getState();
+        if (i == 1)
+            recorder.startRecording();
+        isRecording = true;
+        recordingThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                writeAudioDataToFile();
+            }
+        }, "AudioRecorder Thread");
+        recordingThread.start();
+    }
+
+    private void writeAudioDataToFile() {
+        byte[] data = new byte[bufferSize];
+        String filename = getTempFilename();
+
+        FileOutputStream os = null;
+
+        try {
+            os = new FileOutputStream(filename);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        int read = 0;
+        if (null != os) {
+            while (isRecording) {
+                read = recorder.read(data, 0, bufferSize);
+
+                if (AudioRecord.ERROR_INVALID_OPERATION != read) {
+                    try {
+                        os.write(data);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            try {
+                os.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
     private void stopRecording() {
-        recorder.stop();
+        if (null != recorder) {
+            isRecording = false;
+
+            int i = recorder.getState();
+            if (i == 1)
+                recorder.stop();
+            recorder.release();
+            recorder = null;
+            recordingThread = null;
+            uploadAudio();
+
+        }
+
+        copyWaveFile(getTempFilename(), getFilename());
+
+        deleteTempFile();
+
+    }
 
 
-        recorder.release();
-        recorder = null;
+    private void deleteTempFile() {
+        File file = new File(getTempFilename());
+        file.delete();
+    }
 
-        uploadAudio();
+    private void copyWaveFile(String inFilename, String outFilename) {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        long totalAudioLen = 0;
+        long totalDataLen = totalAudioLen + 36;
+        long longSampleRate = RECORDER_SAMPLERATE;
+        int channels = 2;
+        long byteRate = RECORDER_BPP * RECORDER_SAMPLERATE * channels / 8;
+        byte[] data = new byte[bufferSize];
+
+        try {
+            in = new FileInputStream(inFilename);
+            out = new FileOutputStream(outFilename);
+            totalAudioLen = in.getChannel().size();
+            totalDataLen = totalAudioLen + 36;
+            AppLog.logString("File size:" + totalDataLen);
+            WriteWaveFileHeader(out, totalAudioLen, totalDataLen, longSampleRate, channels, byteRate);
+
+            while (in.read(data) != -1) {
+                out.write(data);
+            }
+            in.close();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private void WriteWaveFileHeader(FileOutputStream out, long totalAudioLen, long totalDataLen, long longSampleRate, int channels, long byteRate)
+            throws IOException {
+
+        byte[] header = new byte[44];
+
+        header[0] = 'R';
+        header[1] = 'I';
+        header[2] = 'F';
+        header[3] = 'F';
+        header[4] = (byte) (totalDataLen & 0xff);
+        header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+        header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+        header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+        header[8] = 'W';
+        header[9] = 'A';
+        header[10] = 'V';
+        header[11] = 'E';
+        header[12] = 'f';
+        header[13] = 'm';
+        header[14] = 't';
+        header[15] = ' ';
+        header[16] = 16;
+        header[17] = 0;
+        header[18] = 0;
+        header[19] = 0;
+        header[20] = 1;
+        header[21] = 0;
+        header[22] = (byte) channels;
+        header[23] = 0;
+        header[24] = (byte) (longSampleRate & 0xff);
+        header[25] = (byte) ((longSampleRate >> 8) & 0xff);
+        header[26] = (byte) ((longSampleRate >> 16) & 0xff);
+        header[27] = (byte) ((longSampleRate >> 24) & 0xff);
+        header[28] = (byte) (byteRate & 0xff);
+        header[29] = (byte) ((byteRate >> 8) & 0xff);
+        header[30] = (byte) ((byteRate >> 16) & 0xff);
+        header[31] = (byte) ((byteRate >> 24) & 0xff);
+        header[32] = (byte) (2 * 16 / 8);
+        header[33] = 0;
+        header[34] = RECORDER_BPP;
+        header[35] = 0;
+        header[36] = 'd';
+        header[37] = 'a';
+        header[38] = 't';
+        header[39] = 'a';
+        header[40] = (byte) (totalAudioLen & 0xff);
+        header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+        header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+        header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+
+
+        out.write(header, 0, 44);
+
+
     }
 
 
     private void uploadAudio() {
         mProgress.setMessage("주문정보를 전송합니다.");
         mProgress.show();
-
-        FirebaseDatabase.getInstance()
-                .getReference().child(uid).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
 
 
         FirebaseDatabase.getInstance().getReference().child(uid).child("recordNumber").setValue(recordNumber);
@@ -261,9 +417,7 @@ public class OrderActivity extends AppCompatActivity {
         StorageReference filepath = mStorage.child("/learning/").child(labelNumber + "_" + recordNumber + ".wav");
 
 
-
-        Uri uri = Uri.fromFile(new File(fileName));
-
+        Uri uri = Uri.fromFile(new File(getFilename()));
         filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
