513cf901c65679d05dfa5063a9593ea937ac170b
seung-aa <baeka0202@gmail.com>
2020-05-07 18:04:08 +0900

    파일수정중!

--- wear/src/main/java/kc/ac/kpu/foruser/MainActivity.java
+++ wear/src/main/java/kc/ac/kpu/foruser/MainActivity.java
@@ -1,49 +1,14 @@
 package kc.ac.kpu.foruser;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
 
+public class MainActivity extends WearableActivity {
 
-    //여기넣으심됩니당
-    TextView textView;
-    Button order_btn;
-    Button menu_btn;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        order_btn = findViewById(R.id.order_wear);
-        menu_btn = findViewById(R.id.menu_wear);
-
-
-        order_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), wearOrderActivity.class);
-                startActivity(intent);
-                finish();
-
-            }
-        });
-
-        menu_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),wearMenuActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-    }
 }
---
+++ wear/src/main/java/kc/ac/kpu/foruser/MenuActivity.java
@@ -0,0 +1,41 @@
+package kc.ac.kpu.foruser;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import kc.ac.kpu.wear.R;
+
+
+public class MenuActivity extends WearableActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        // Enables Always-on
+        setAmbientEnabled();
+
+        mTextView = (TextView) findViewById(R.id.text);
+        Button btn = (Button)findViewById(R.id.order_btn);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+        }
+
+});
+}
+
+
+    }
+
